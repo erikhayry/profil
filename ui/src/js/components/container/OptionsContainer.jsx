2faf5d37231cfdbc8b1a998691acf440a6367a1c
Erik Portin <erik.portin@svt.se>
2019-11-22 14:27:02 +0100

    avatars

--- ui/src/js/components/container/OptionsContainer.jsx
+++ ui/src/js/components/container/OptionsContainer.jsx
@@ -1,6 +1,7 @@
 import browser from "webextension-polyfill";
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Avatar from 'avataaars'
 
 class OptionsContainer extends Component {
     constructor() {
@@ -39,6 +40,23 @@ class OptionsContainer extends Component {
             <>
                 <div className="page-container">
                     <h1>Profiler</h1>
+                    <div>
+                        Your avatar:
+                        <Avatar
+                            style={{width: '100px', height: '100px'}}
+                            avatarStyle='Circle'
+                            topType='LongHairMiaWallace'
+                            accessoriesType='Prescription02'
+                            hairColor='BrownDark'
+                            facialHairType='Blank'
+                            clotheType='Hoodie'
+                            clotheColor='PastelBlue'
+                            eyeType='Happy'
+                            eyebrowType='Default'
+                            mouthType='Smile'
+                            skinColor='Light'
+                        />
+                    </div>
                     <p>user: {user}</p>
                     <button disabled={user === 1} onClick={() =>
                         this.setUser(1)
